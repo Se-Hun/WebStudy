6eba45a606d25d280b1f6f28ea84b2f910c68e6c
SeHunHu <tpgns5247@gmail.com>
2019-01-19 02:15:51 +0900

    1.19 공부

---
+++ React.js/code/(16장)App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+class App extends Component {
+
+  state = {
+  }
+
+  componentDidMount() {
+    this._getMovies()
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
+      </div>
+    );
+  }
+}
+
+export default App;
